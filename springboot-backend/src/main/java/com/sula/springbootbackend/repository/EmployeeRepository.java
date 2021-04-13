a9fb623cfae74d36672c407d07e46052fc84c197
sulakkhanaid <sulakkhana@deeplab.lk>
2021-04-13 17:54:11 +0530

    first commit

---
+++ springboot-backend/src/main/java/com/sula/springbootbackend/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.sula.springbootbackend.repository;
+
+import com.sula.springbootbackend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}
